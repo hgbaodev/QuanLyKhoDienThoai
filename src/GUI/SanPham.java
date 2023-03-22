15a3bef9f17e6e4b14a636500bbfa0c3ff1e4c49
hgbaodev <120194990+hgbaodev@users.noreply.github.com>
2023-03-22 10:51:46 +0700

    Update SanPham.java

--- src/GUI/SanPham.java
+++ src/GUI/SanPham.java
@@ -53,11 +53,10 @@ public class SanPham extends JPanel {
 
         // functionBar là thanh bên trên chứa các nút chức năng như thêm xóa sửa, và tìm kiếm
         functionBar = new PanelBorderRadius();
-        functionBar.setPreferredSize(new Dimension(0, 180));
-        functionBar.setLayout(new FlowLayout(1, 15, 40));
+        functionBar.setPreferredSize(new Dimension(0, 100));
+        functionBar.setLayout(new GridLayout(1,2));
 
         mainFunction = new MainFunction();
-        mainFunction.setLayout(new GridLayout(1,2));
         functionBar.add(mainFunction);
 
         search = new IntegratedSearch();
