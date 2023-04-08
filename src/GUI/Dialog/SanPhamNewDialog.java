9e390651a0de0681fc6079d497e8958c477c7b01
Trần Nhật Sinh <transinh085@gmail.com>
2023-04-08 22:21:52 +0700

    up

--- src/GUI/Dialog/SanPhamNewDialog.java
+++ src/GUI/Dialog/SanPhamNewDialog.java
@@ -4,12 +4,26 @@
  */
 package GUI.Dialog;
 
+import GUI.Component.ButtonCustom;
+import GUI.Component.HeaderTitle;
+import GUI.Component.InputForm;
+import GUI.Component.InputImage;
+import GUI.Component.SelectForm;
 import javax.swing.JDialog;
+import javax.swing.JPanel;
 
 /**
  *
  * @author Tran Nhat Sinh
  */
 public class SanPhamNewDialog extends JDialog{
+    private HeaderTitle titlePage;
+    private JPanel pnmain, pnbottom, pnCenter, pnmainright;
+    private ButtonCustom btnThemSanPham, btnCapNhat, btnHuyBo;
+    InputForm tenSP, xuatxu, gianhap, giaxuat, soluong, idSP;
+    SelectForm thuonghieu, khuvuc;
+    InputImage hinhanh;
+    String urlImage;
+    
     
 }
