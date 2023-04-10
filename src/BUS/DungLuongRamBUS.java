c14aa76645f36eb700d3e1debd41ea0de596e914
Trần Nhật Sinh <transinh085@gmail.com>
2023-04-10 21:07:01 +0700

    upo

--- src/BUS/DungLuongRamBUS.java
+++ src/BUS/DungLuongRamBUS.java
@@ -4,10 +4,31 @@
  */
 package BUS;
 
+import DAO.DungLuongRamDAO;
+import DTO.ThuocTinhSanPham.DungLuongRamDTO;
+import java.util.ArrayList;
+
 /**
  *
  * @author Tran Nhat Sinh
  */
 public class DungLuongRamBUS {
+    private DungLuongRamDAO dungluongramDAO = new DungLuongRamDAO();
+    private ArrayList<DungLuongRamDTO> listRam = new ArrayList<>();
+
+    public DungLuongRamBUS() {
+        this.listRam = dungluongramDAO.selectAll();
+    }
     
+    public ArrayList<DungLuongRamDTO> getAll() {
+        return this.listRam;
+    }
+    
+    public String[] getArrKichThuoc() {
+        String[] result = new String[listRam.size()];
+        for(int i = 0; i < listRam.size(); i++) {
+            result[i] = Integer.toString(listRam.get(i).getDungluongram());
+        }
+        return result;
+    }
 }
--- src/BUS/DungLuongRomBUS.java
+++ src/BUS/DungLuongRomBUS.java
@@ -4,10 +4,31 @@
  */
 package BUS;
 
+import DAO.DungLuongRomDAO;
+import DTO.ThuocTinhSanPham.DungLuongRomDTO;
+import java.util.ArrayList;
+
 /**
  *
  * @author Tran Nhat Sinh
  */
 public class DungLuongRomBUS {
+    private DungLuongRomDAO dungluongramDAO = new DungLuongRomDAO();
+    private ArrayList<DungLuongRomDTO> listRom = new ArrayList<>();
+
+    public DungLuongRomBUS() {
+        this.listRom = dungluongramDAO.selectAll();
+    }
     
+    public ArrayList<DungLuongRomDTO> getAll() {
+        return this.listRom;
+    }
+    
+    public String[] getArrKichThuoc() {
+        String[] result = new String[listRom.size()];
+        for(int i = 0; i < listRom.size(); i++) {
+            result[i] = Integer.toString(listRom.get(i).getDungluongrom());
+        }
+        return result;
+    }
 }
