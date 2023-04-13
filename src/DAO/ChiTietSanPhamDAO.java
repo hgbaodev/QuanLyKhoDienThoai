bd6ad6c7d22925a51235d7af577d5898a93f67ac
Trần Nhật Sinh <transinh085@gmail.com>
2023-04-13 23:34:27 +0700

    up

--- src/DAO/ChiTietSanPhamDAO.java
+++ src/DAO/ChiTietSanPhamDAO.java
@@ -1,27 +1,72 @@
 package DAO;
 
 import DTO.ChiTietSanPhamDTO;
+import config.JDBCUtil;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Tran Nhat Sinh
  */
-public class ChiTietSanPhamDAO implements DAOinterface<ChiTietSanPhamDTO>{
+public class ChiTietSanPhamDAO implements DAOinterface<ChiTietSanPhamDTO> {
 
     @Override
     public int insert(ChiTietSanPhamDTO t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int result = 0;
+        try {
+            Connection con = (Connection) JDBCUtil.getConnection();
+            String sql = "INSERT INTO `ctsanpham`(`maimei`, `maphienbansp`, `maphieunhap`, `tinhtrang`) VALUES (?,?,?,?)";
+            PreparedStatement pst = (PreparedStatement) con.prepareStatement(sql);
+            pst.setString(1, t.getImei());
+            pst.setInt(2, t.getMaphienbansp());
+            pst.setInt(3, t.getMaphieunhap());
+            pst.setInt(4, t.getTinhtrang());
+            result = pst.executeUpdate();
+            JDBCUtil.closeConnection(con);
+        } catch (SQLException ex) {
+            Logger.getLogger(ChiTietSanPhamDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
     }
 
     @Override
     public int update(ChiTietSanPhamDTO t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int result = 0;
+        try {
+            Connection con = (Connection) JDBCUtil.getConnection();
+            String sql = "UPDATE `ctsanpham` SET `maphienbansp`=?,`maphieunhap`=?,`maphieuxuat`=?,`tinhtrang`=? WHERE `maimei`=?";
+            PreparedStatement pst = (PreparedStatement) con.prepareStatement(sql);
+            pst.setInt(1, t.getMaphienbansp());
+            pst.setInt(2, t.getMaphieunhap());
+            pst.setInt(3, t.getMaphieuxuat());
+            pst.setInt(4, t.getTinhtrang());
+            pst.setString(5, t.getImei());
+            result = pst.executeUpdate();
+            JDBCUtil.closeConnection(con);
+        } catch (SQLException ex) {
+            Logger.getLogger(ChiTietSanPhamDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
     }
 
     @Override
     public int delete(String t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        int result = 0;
+        try {
+            Connection con = (Connection) JDBCUtil.getConnection();
+            String sql = "UPDATE `ctsanpham` SET `tinhtrang` = 0 WHERE  maimei = ?";
+            PreparedStatement pst = (PreparedStatement) con.prepareStatement(sql);
+            result = pst.executeUpdate();
+            JDBCUtil.closeConnection(con);
+        } catch (SQLException ex) {
+            Logger.getLogger(KhuVucKhoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
     }
 
     @Override
@@ -38,5 +83,5 @@ public class ChiTietSanPhamDAO implements DAOinterface<ChiTietSanPhamDTO>{
     public int getAutoIncrement() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-    
+
 }
