07db0b8856da73335f63434b5fa483122858138c
hgbaodev <120194990+hgbaodev@users.noreply.github.com>
2023-04-30 14:26:57 +0700

    up

---
+++ src/GUI/Component/CheckListRenderer.java
@@ -0,0 +1,20 @@
+package GUI.Component;
+
+import java.awt.Component;
+import javax.swing.JCheckBox;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+public class CheckListRenderer extends JCheckBox implements ListCellRenderer {
+
+    public Component getListCellRendererComponent(JList list, Object value,
+            int index, boolean isSelected, boolean hasFocus) {
+        setEnabled(list.isEnabled());
+        setSelected(((CheckListItem) value).isSelected());
+        setFont(list.getFont());
+        setBackground(list.getBackground());
+        setForeground(list.getForeground());
+        setText(value.toString());
+        return this;
+    }
+}
