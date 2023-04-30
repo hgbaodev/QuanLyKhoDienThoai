58a862af1ec322ef45933998f7db2995404046a0
Trần Nhật Sinh <transinh085@gmail.com>
2023-04-30 23:28:55 +0700

    update

--- src/GUI/Dialog/ChiTietPhieuDialog.java
+++ src/GUI/Dialog/ChiTietPhieuDialog.java
@@ -6,18 +6,23 @@ import BUS.MauSacBUS;
 import BUS.PhienBanSanPhamBUS;
 import BUS.PhieuNhapBUS;
 import BUS.SanPhamBUS;
+import DAO.NhaCungCapDAO;
+import DAO.NhanVienDAO;
 import DTO.ChiTietPhieuNhapDTO;
 import DTO.ChiTietSanPhamDTO;
 import DTO.PhienBanSanPhamDTO;
 import DTO.PhieuNhapDTO;
+import GUI.Component.ButtonCustom;
 import GUI.Component.HeaderTitle;
 import GUI.Component.InputForm;
-import GUI.Component.SelectForm;
 import helper.Formater;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
@@ -31,12 +36,11 @@ import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumnModel;
 
-public class ChiTietPhieuDialog extends JDialog {
+public final class ChiTietPhieuDialog extends JDialog implements ActionListener {
 
     HeaderTitle titlePage;
-    JPanel pnmain, pnmain_top, pnmain_bottom, pnmain_bottom_right, pnmain_bottom_left;
+    JPanel pnmain, pnmain_top, pnmain_bottom, pnmain_bottom_right, pnmain_bottom_left, pnmain_btn;
     InputForm txtMaPhieu, txtNhanVien, txtNhaCungCap, txtThoiGian;
     DefaultTableModel tblModel, tblModelImei;
     JTable table, tblImei;
@@ -48,6 +52,7 @@ public class ChiTietPhieuDialog extends JDialog {
     DungLuongRomBUS romBus = new DungLuongRomBUS();
     MauSacBUS mausacBus = new MauSacBUS();
     SanPhamBUS sanPhamBUS = new SanPhamBUS();
+    ButtonCustom btnPdf, btnHuyBo;
 
     ArrayList<ChiTietPhieuNhapDTO> chitietphieu;
     HashMap<Integer, ArrayList<ChiTietSanPhamDTO>> chitietsanpham = new HashMap<>();
@@ -65,8 +70,8 @@ public class ChiTietPhieuDialog extends JDialog {
 
     public void initPhieuNhap() {
         txtMaPhieu.setText("PN" + Integer.toString(this.phieunhap.getMaphieu()));
-        txtNhaCungCap.setText(phieunhap.getManhacungcap() + "");
-        txtNhanVien.setText(phieunhap.getManguoitao() + "");
+        txtNhaCungCap.setText(NhaCungCapDAO.getInstance().selectById(phieunhap.getManhacungcap() + "").getTenncc());
+        txtNhanVien.setText(NhanVienDAO.getInstance().selectById(phieunhap.getManguoitao() + "").getHoten());
         txtThoiGian.setText(Formater.FormatTime(phieunhap.getThoigiantao()));
     }
 
@@ -82,11 +87,11 @@ public class ChiTietPhieuDialog extends JDialog {
             });
         }
     }
-    
+
     public void loadDataTableImei(ArrayList<ChiTietSanPhamDTO> dssp) {
         tblModelImei.setRowCount(0);
         int size = dssp.size();
-        for(int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) {
             tblModelImei.addRow(new Object[]{
                 i + 1, dssp.get(i).getImei()
             });
@@ -94,7 +99,7 @@ public class ChiTietPhieuDialog extends JDialog {
     }
 
     public void initComponent(String title) {
-        this.setSize(new Dimension(1100, 460));
+        this.setSize(new Dimension(1100, 500));
         this.setLayout(new BorderLayout(0, 0));
         titlePage = new HeaderTitle(title.toUpperCase());
 
@@ -131,7 +136,6 @@ public class ChiTietPhieuDialog extends JDialog {
         scrollTable.setViewportView(table);
         DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
         centerRenderer.setHorizontalAlignment(JLabel.CENTER);
-        TableColumnModel columnModel = table.getColumnModel();
         table.setDefaultRenderer(Object.class, centerRenderer);
         table.addMouseListener(new MouseAdapter() {
             @Override
@@ -160,11 +164,30 @@ public class ChiTietPhieuDialog extends JDialog {
         pnmain_bottom.add(pnmain_bottom_left, BorderLayout.CENTER);
         pnmain_bottom.add(pnmain_bottom_right, BorderLayout.EAST);
 
+        pnmain_btn = new JPanel(new FlowLayout());
+        pnmain_btn.setBorder(new EmptyBorder(10, 0, 10, 0));
+        pnmain_btn.setBackground(Color.white);
+        btnPdf = new ButtonCustom("Xuất file PDF", "success", 14);
+        btnHuyBo = new ButtonCustom("Huỷ bỏ", "danger", 14);
+        btnPdf.addActionListener(this);
+        btnHuyBo.addActionListener(this);
+        pnmain_btn.add(btnPdf);
+        pnmain_btn.add(btnHuyBo);
+
         pnmain.add(pnmain_top, BorderLayout.NORTH);
         pnmain.add(pnmain_bottom, BorderLayout.CENTER);
+        pnmain.add(pnmain_btn, BorderLayout.SOUTH);
 
         this.add(titlePage, BorderLayout.NORTH);
         this.add(pnmain, BorderLayout.CENTER);
         this.setLocationRelativeTo(null);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        if (source == btnHuyBo) {
+            dispose();
+        }
     }
 }
