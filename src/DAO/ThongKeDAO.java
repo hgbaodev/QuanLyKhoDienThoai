7eb306af415ea06448aa2fc886639b255b6504a7
Đinh Ngọc Ân <andinh1443@gmail.com>
2023-05-03 11:11:00 +0700

    up

--- src/DAO/ThongKeDAO.java
+++ src/DAO/ThongKeDAO.java
@@ -4,10 +4,34 @@
  */
 package DAO;
 
+import DTO.KhachHangDTO;
+import DTO.PhieuXuatDTO;
+import DTO.ThongKe.ThongKeKhachHangDTO;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
 /**
  *
  * @author Tran Nhat Sinh
  */
 public class ThongKeDAO {
-    
+    public static ThongKeDAO getInstance(){
+        return new ThongKeDAO();
+    }
+
+    public ArrayList<ThongKeKhachHangDTO> thongkeKH() {
+        ArrayList<KhachHangDTO> listKh = KhachHangDAO.getInstance().selectAll();
+        ArrayList<ThongKeKhachHangDTO> tkkh = new ArrayList<>();
+        for (KhachHangDTO kh : listKh) {
+            ArrayList<PhieuXuatDTO> px = PhieuXuatDAO.getInstance().selectAllofKH(kh.getMaKH());
+            int count = 0, total = 0;
+            for (PhieuXuatDTO i : px) {
+                total += i.getTongTien();
+                count++;
+            }
+            tkkh.add(new ThongKeKhachHangDTO(kh.getMaKH(),kh.getHoten(),count,total));
+        }
+        return tkkh;
+    }
+
 }
