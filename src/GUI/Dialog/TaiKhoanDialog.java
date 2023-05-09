80f904759c626adf300c52cb99432153bc732663
ChinhCong <chinhdnc1@gmail.com>
2023-05-09 10:20:07 +0700

    UP

--- src/GUI/Dialog/TaiKhoanDialog.java
+++ src/GUI/Dialog/TaiKhoanDialog.java
@@ -27,11 +27,13 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
 /**
  *
  * @author robot
  */
-public class TaiKhoanDialog extends JDialog{
+public class TaiKhoanDialog extends JDialog {
+
     private TaiKhoan taiKhoan;
     private TaiKhoan jbTaiKhoan;
     private HeaderTitle titlePage;
@@ -43,8 +45,9 @@ public class TaiKhoanDialog extends JDialog{
     private SelectForm trangthai;
     int manv;
     private ArrayList<NhomQuyenDTO> listNq = NhomQuyenDAO.getInstance().selectAll();
-    
-    public TaiKhoanDialog(TaiKhoan taiKhoan, JFrame owner, String title, boolean modal, String type, int manv){
+    private ArrayList<TaiKhoanDTO> listTK = TaiKhoanDAO.getInstance().selectAll();
+
+    public TaiKhoanDialog(TaiKhoan taiKhoan, JFrame owner, String title, boolean modal, String type, int manv) {
         super(owner, title, modal);
         init(title, type);
         this.manv = manv;
@@ -52,30 +55,30 @@ public class TaiKhoanDialog extends JDialog{
         setLocationRelativeTo(null);
         setVisible(true);
     }
-    
-    public TaiKhoanDialog(TaiKhoan taiKhoan, JFrame owner, String title, boolean modal, String type, TaiKhoanDTO tk){
+
+    public TaiKhoanDialog(TaiKhoan taiKhoan, JFrame owner, String title, boolean modal, String type, TaiKhoanDTO tk) {
         super(owner, title, modal);
         init(title, type);
         this.manv = tk.getManv();
         this.taiKhoan = taiKhoan;
         username.setText(tk.getUsername());
         password.setPass(tk.getMatkhau());
-        maNhomQuyen.setSelectedItem(NhomQuyenDAO.getInstance().selectById(tk.getManhomquyen()+"").getTennhomquyen());
+        maNhomQuyen.setSelectedItem(NhomQuyenDAO.getInstance().selectById(tk.getManhomquyen() + "").getTennhomquyen());
         trangthai.setSelectedIndex(tk.getTrangthai());
         setLocationRelativeTo(null);
         setVisible(true);
     }
-    
-    public void init(String title, String type){
+
+    public void init(String title, String type) {
         this.setSize(new Dimension(500, 620));
         this.setLayout(new BorderLayout(0, 0));
         titlePage = new HeaderTitle(title.toUpperCase());
         pnmain = new JPanel(new GridLayout(4, 1, 5, 0));
         pnmain.setBackground(Color.white);
         username = new InputForm("Tên đăng nhập");
-        password = new InputForm("Mật khẩu","password");
+        password = new InputForm("Mật khẩu", "password");
         maNhomQuyen = new SelectForm("Nhóm quyền", getNhomQuyen());
-        trangthai = new SelectForm("Trạng thái", new String[]{"Ngưng hoạt động","Hoạt động"});
+        trangthai = new SelectForm("Trạng thái", new String[]{"Ngưng hoạt động", "Hoạt động"});
         pnmain.add(username);
         pnmain.add(password);
         pnmain.add(maNhomQuyen);
@@ -86,49 +89,64 @@ public class TaiKhoanDialog extends JDialog{
         btnThem = new ButtonCustom("Thêm tài khoản", "success", 14);
         btnCapNhat = new ButtonCustom("Lưu thông tin", "success", 14);
         btnHuyBo = new ButtonCustom("Huỷ bỏ", "danger", 14);
-        btnThem.addActionListener(new ActionListener(){
+
+        btnThem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(validateInput()){
-                String tendangnhap = username.getText();
-                String pass = BCrypt.hashpw(password.getPass(), BCrypt.gensalt(12));
-                int manhom = listNq.get(maNhomQuyen.getSelectedIndex()).getManhomquyen();
-                int tt = trangthai.getSelectedIndex();
-                TaiKhoanDTO tk = new TaiKhoanDTO(manv, tendangnhap, pass, manhom, tt);
-                TaiKhoanDAO.getInstance().insert(tk);
-                taiKhoan.taiKhoanBus.addAcc(tk);
-                taiKhoan.loadTable(taiKhoan.taiKhoanBus.getTaiKhoanAll());
-                dispose();
+                if (validateInput()) {
+                    String tendangnhap = username.getText();
+                    int check = 0;
+                    for (TaiKhoanDTO i : listTK) {
+                        if (i.getUsername().equals(username.getText())) {
+                            check++;
+                            break;
+                        }
+                    }
+                    if (check == 0) {
+                        String pass = BCrypt.hashpw(password.getPass(), BCrypt.gensalt(12));
+                        int manhom = listNq.get(maNhomQuyen.getSelectedIndex()).getManhomquyen();
+                        int tt = trangthai.getSelectedIndex();
+                        TaiKhoanDTO tk = new TaiKhoanDTO(manv, tendangnhap, pass, manhom, tt);
+                        TaiKhoanDAO.getInstance().insert(tk);
+                        taiKhoan.taiKhoanBus.addAcc(tk);
+                        taiKhoan.loadTable(taiKhoan.taiKhoanBus.getTaiKhoanAll());
+                        dispose();
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Tên tài khoản đã tồn tại. Vui lòng đổi tên khác!", "Cảnh báo!", JOptionPane.WARNING_MESSAGE);
+                        username.getFocusCycleRootAncestor();
+                    }
+
                 }
             }
         });
-        btnCapNhat.addActionListener(new ActionListener(){
+        btnCapNhat.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(!(username.getText().length()==0)){
-                String tendangnhap = username.getText();
-                String pass = BCrypt.hashpw(password.getPass(), BCrypt.gensalt(12));
-                int manhom = listNq.get(maNhomQuyen.getSelectedIndex()).getManhomquyen();
-                int tt = trangthai.getSelectedIndex();
-                TaiKhoanDTO tk = new TaiKhoanDTO(manv, tendangnhap, pass, manhom, tt);
-                TaiKhoanDAO.getInstance().update(tk);
-                taiKhoan.taiKhoanBus.updateAcc(taiKhoan.getRowSelected(), tk);
-                taiKhoan.loadTable(taiKhoan.taiKhoanBus.getTaiKhoanAll());
-                dispose();
+                if (!(username.getText().length() == 0)) {
+                    String tendangnhap = username.getText();
+                    String pass = BCrypt.hashpw(password.getPass(), BCrypt.gensalt(12));
+                    int manhom = listNq.get(maNhomQuyen.getSelectedIndex()).getManhomquyen();
+                    int tt = trangthai.getSelectedIndex();
+                    TaiKhoanDTO tk = new TaiKhoanDTO(manv, tendangnhap, pass, manhom, tt);
+                    TaiKhoanDAO.getInstance().update(tk);
+                    taiKhoan.taiKhoanBus.updateAcc(taiKhoan.getRowSelected(), tk);
+                    taiKhoan.loadTable(taiKhoan.taiKhoanBus.getTaiKhoanAll());
+                    dispose();
                 } else {
                     JOptionPane.showMessageDialog(null, "Vui lòng không để trống tên");
                 }
             }
         });
-        btnHuyBo.addActionListener(new ActionListener(){
+        btnHuyBo.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 dispose();
             }
         });
-        
+
         switch (type) {
-            case "create" -> pnbottom.add(btnThem);
+            case "create" ->
+                pnbottom.add(btnThem);
             case "update" -> {
                 pnmain.remove(password);
                 pnbottom.add(btnCapNhat);
@@ -142,33 +160,34 @@ public class TaiKhoanDialog extends JDialog{
                 trangthai.setDisable();
                 this.setSize(new Dimension(500, 550));
             }
-            default -> throw new AssertionError();
+            default ->
+                throw new AssertionError();
         }
         pnbottom.add(btnHuyBo);
         this.add(titlePage, BorderLayout.NORTH);
         this.add(pnmain, BorderLayout.CENTER);
         this.add(pnbottom, BorderLayout.SOUTH);
     }
-    
-    public String[] getNhomQuyen(){
+
+    public String[] getNhomQuyen() {
         String[] listNhomQuyen = new String[listNq.size()];
-        for(int i = 0; i < listNq.size(); i++){
+        for (int i = 0; i < listNq.size(); i++) {
             listNhomQuyen[i] = listNq.get(i).getTennhomquyen();
         }
         return listNhomQuyen;
     }
-    
-    public boolean validateInput(){
-        if(username.getText().length()==0){
+
+    public boolean validateInput() {
+        if (username.getText().length() == 0) {
             JOptionPane.showMessageDialog(this, "Vui lòng không để trống tên đăng nhập");
             return false;
-        } else if(username.getText().length()<6){
+        } else if (username.getText().length() < 6) {
             JOptionPane.showMessageDialog(this, "Tên đăng nhập ít nhất 6 kí tự");
             return false;
-        } else if (password.getPass().length()==0){
+        } else if (password.getPass().length() == 0) {
             JOptionPane.showMessageDialog(this, "Vui lòng không để trống mật khẩu");
             return false;
-        } else if(password.getPass().length()<6){
+        } else if (password.getPass().length() < 6) {
             JOptionPane.showMessageDialog(this, "Mật khẩu ít nhất 6 ký tự");
             return false;
         }
