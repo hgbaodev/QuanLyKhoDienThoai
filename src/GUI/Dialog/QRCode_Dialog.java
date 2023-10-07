84c7403cc3286733e3e122c346d65894e3f16a42
Hoàng Gia Bảo <120194990+hgbaodev@users.noreply.github.com>
2023-10-07 09:15:58 +0700

    Update QRCode_Dialog.java

--- src/GUI/Dialog/QRCode_Dialog.java
+++ src/GUI/Dialog/QRCode_Dialog.java
@@ -21,11 +21,8 @@ import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.ThreadFactory;
 import GUI.Component.ButtonCustom;
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.logging.Level;
@@ -124,9 +121,9 @@ public class QRCode_Dialog extends JDialog {
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
+                dispose();
                 wCamPanel.stop();
                 t.stop();
-                dispose();
             }
         });
     }
